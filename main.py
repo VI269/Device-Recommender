023dc23b183af3ab0c6fdc6241da65e6609f7423
Vedant Iyer <edu.vedant01@gmail.com>
2022-10-15 17:29:14 +0800

    Changes

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import json
-
 def ask_the_questions(file):
     with open(file, 'r') as f:
         device_data = f.readlines()[0]
@@ -9,8 +7,26 @@ def ask_the_questions(file):
         outputs.append(input(question+" "))
     return outputs
 
+def remove_new_lines(array):
+    out = []
+    for item in array:
+        out.append(item[:-2])
+    return out
+
 def process(questionnaire_results, file):
-    pass
+    with open(file) as f:
+        outs = []
+        questions = remove_new_lines(f.readlines()[1:])
+        for _ in questions:
+            for exp in _.split(" | ")[1].split(","):
+                for response in questionnaire_results:
+                    if exp == response:
+                        outs.append(_.split(' | ')[0])
+        if len(outs) == 0:
+            matches = {}
+            for _ in questions:
+                for exp in _.split(" | ")[1].split(","):
+                    
 
 if __name__ == '__main__':
-    print(process(ask_the_questions('device.json'), 'device.json'))
+    print(process("", "device.txt"))
