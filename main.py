b68c7a98afc4fc635322582444855d96b08794d7
Vedant Iyer <edu.vedant01@gmail.com>
2022-10-15 17:15:36 +0800

    Code

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import json
 
+def ask_the_questions(file):
+    with open(file, 'r') as f:
+        device_data = f.readlines()[0]
+        questions = device_data.split(' | ')
+    outputs = []
+    for question in questions.split(','):
+        outputs.append(input(question+" "))
+    return outputs
 
+def process(questionnaire_results, file):
+    pass
+
+if __name__ == '__main__':
+    print(process(ask_the_questions('device.json'), 'device.json'))
